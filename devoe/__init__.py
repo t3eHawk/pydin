34827441123f87054e870f3dcd887f2a1c7f9ca6
t3eHawk <timurfaradzhov@gmail.com>
2022-04-30 02:42:59 +0300

    Initialize version 0.0.2

--- devoe/__init__.py
+++ devoe/__init__.py
@@ -1,17 +1,24 @@
 """Python ETL framework."""
 
 from .cli import Manager
-from .core import Scheduler, Job
-from .web import WEBAPI
+from .core import Scheduler, Job, Task, Pipeline
+from .web import API
 
+from .api import Operator
+from .items import Mapper
+from .items import Table, SQL, Select, Insert
+from .items import CSV, JSON, XML
+from .items import Files, FileManager
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __status__ = 'Development'
 
 __author__ = 'Timur Faradzhov'
 __email__ = 'timurfaradzhov@gmail.com'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2020, The Devoe Project'
+__copyright__ = 'Copyright 2022, The Devoe Project'
 __maintainer__ = __author__
 
-__all__ = [Manager, Scheduler, Job, WEBAPI]
+__all__ = [Manager, Operator, Scheduler, Job, Task, Pipeline, API,
+           Mapper, Table, SQL, Select, Insert, CSV, JSON, XML, Files,
+           FileManager]
